589976f247c5d77fb6cfa6ac113fb35a3f137350
Zeyad Altaji <ziadaltaji130@gmail.com>
2022-09-16 10:57:37 +0300

    add to seedDemoData

---
+++ WebAPIS/Migrations/20220916075617_seedDemoData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPIS.Migrations
+{
+    public partial class seedDemoData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
