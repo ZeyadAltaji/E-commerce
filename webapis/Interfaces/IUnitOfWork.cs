4f34be3650c86acbaf36f8054107510d1a7edd36
Zeyad Altaji <ziadaltaji130@gmail.com>
2022-09-23 18:00:12 +0300

    Correct some errors

--- webapis/Interfaces/IUnitOfWork.cs
+++ webapis/Interfaces/IUnitOfWork.cs
@@ -11,6 +11,8 @@ namespace WebAPIS.Interfaces
         IUserReop UserRepository { get; }
         IPropertyRepository propertyRepository { get; }
         IPropertyTypeRepository PropertyTypeRepository { get; }
+        IFurnishingTypeRepository FurnishingTypeRepository { get; }
+
         Task<bool> SaveAsync();
     }
 }
